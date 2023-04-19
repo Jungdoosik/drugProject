854469cdc76b376bddb5440bdb63290ea3a403e3
haeri <evergreen0208@hanmail.net>
2023-04-19 17:18:24 +0900

    회원가입 중복 체크 수정

--- src/main/webapp/resources/js/member.js
+++ src/main/webapp/resources/js/member.js
@@ -49,13 +49,13 @@ function joinChk() {
 	} else if (pwd != pwdChk) {
 		alert("입력하신 비밀번호와 비밀번호 확인이 상이합니다. 다시 확인하여 주시기 바랍니다.");
 		val = false;
-	} /*else if (idStatus == "no") {
+	} else if (idStatus == "no") {
 		alert("중복된 아이디입니다.");
 		val = false;
 	} else if (idStatus == "del") {
 		alert("탈퇴한 아이디입니다.(사용불가)");
 		val = false;
-	}*/
+	}
 
 	if (val == false) {
 		return false;
