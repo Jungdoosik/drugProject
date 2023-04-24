db122513b39ca3e0add7cdcc18ac8172e1455805
DESKTOP-VNR25FI\USER <k_crown_@naver.com>
2023-04-25 00:17:06 +0900

    캘린더 TEST 2

--- src/main/java/com/itkey/controller/ScheduleController.java
+++ src/main/java/com/itkey/controller/ScheduleController.java
@@ -1,9 +1,17 @@
 package com.itkey.controller;
 
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ScheduleController {
@@ -14,5 +22,18 @@ public class ScheduleController {
 	    public String calendar() {
 	    	return "calendar";
 	    }
+	    
+
+	    @PostMapping(value = "/enrollCalendar")
+	    public @ResponseBody String enrollCal(@RequestParam("cal_data") String cal_data, @RequestParam("memo") String memo ) throws ParseException {
+	    	logger.debug("enrollCalendar is Running...");
+	    	logger.debug("cal_data >>> "+ cal_data);
+	    	logger.debug("memo >>> "+ memo);
+	    	
+	    	//db로 insert 로직 추가해야함
+	    	//service
+	    	return "enrollCalendar";
+	    }
+	    
+	    
 }
- 
